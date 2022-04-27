da08bfdfa130d9ee49957a3921d1fd5dd07784f6
LiuKairui <tclkr@live.com>
2022-04-27 21:34:44 +0800

    fix: ass7

--- Assignment7/code/Scene.cpp
+++ Assignment7/code/Scene.cpp
@@ -4,7 +4,7 @@
 
 #include "Scene.hpp"
 
-const float eps = 0.01;
+const float eps = 0.0005f;
 
 void Scene::buildBVH() {
     printf(" - Generating BVH...\n\n");
@@ -61,14 +61,12 @@ bool Scene::trace(
 // Implementation of Path Tracing
 Vector3f Scene::castRay(const Ray &ray, int depth) const
 {
-
-    auto hit_p = intersect(ray);
     
-    if(!hit_p.happened) return Vector3f{0, 0, 0};
-    // if(hit_p.m->hasEmission()) return hit_p.m->getEmission();
-    if(hit_p.m->hasEmission()) return hit_p.emit;
+    auto hit_p = intersect(ray);
+	if (!hit_p.happened) return Vector3f(0,0,0);
+    if(hit_p.m->hasEmission()) return hit_p.m->getEmission();
 
-    auto n_p = hit_p.normal;
+    auto n_p = hit_p.normal.normalized();
     auto w_o = -ray.direction;
     auto pos_p = hit_p.coords;
 
@@ -77,19 +75,29 @@ Vector3f Scene::castRay(const Ray &ray, int depth) const
     sampleLight(hit_x,pdf_l);
     auto pos_x = hit_x.coords;
     auto xp = (pos_p - pos_x);
-    auto w_i_inv = normalize(xp);
-    auto w_i = -w_i_inv;
+    auto w_i_inv = xp.normalized();
+    auto w_i_inv2 = normalize(xp);
+    auto w_i = (-xp).normalized();
     auto n_x = hit_x.normal;
 
     Vector3f l_indir(0,0,0), l_dir(0,0,0);
-    if((intersect(Ray(pos_p, w_i)).coords - pos_x).norm() < eps) {
-        l_dir = hit_x.emit * hit_p.m->eval(w_o, w_i, n_p) * dotProduct(w_i, n_p) * dotProduct(-w_i_inv, n_x) / (((-xp).norm() * (-xp).norm()) * pdf_l);
+
+    if(intersect(Ray(pos_p, w_i)).distance - (-xp).norm() > -eps) {
+        float cos1 = std::max(dotProduct(w_i, n_p), 0.0f);
+        float cos2 = std::max(dotProduct(w_i_inv, n_x), 0.0f);
+        l_dir = hit_x.emit * hit_p.m->eval(w_i, w_o, n_p) * cos1 * cos2 / (dotProduct(-xp, -xp) * pdf_l);
     }
 
     if(get_random_float()<RussianRoulette){
-        Vector3f w_indir = hit_p.m->sample(w_o, n_p);
-        l_indir = castRay(Ray(pos_p, w_indir), depth) * hit_p.m->eval(w_indir, w_o, n_p) * dotProduct(w_indir, n_p) /
-            (hit_p.m->pdf(w_indir, w_o, n_p) * Scene::RussianRoulette);
+        Vector3f w_indir = hit_p.m->sample(w_o, n_p).normalized();
+        auto pdf_indir = hit_p.m->pdf(w_o, w_indir, n_p);
+        if(pdf_indir>eps){
+            auto nextHit = intersect(Ray(hit_p.coords, w_indir));
+            if (nextHit.happened && !nextHit.m->hasEmission()){
+                auto cost = std::max(.0f, dotProduct(w_indir, n_p));
+                l_indir = 1.0*castRay(Ray(pos_p, w_indir), depth+1) * hit_p.m->eval(w_indir, w_o, n_p) * cost / (pdf_indir * Scene::RussianRoulette);
+            }
+        }
     }
 
     return l_indir+l_dir;
